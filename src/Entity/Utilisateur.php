f2dfece81785c1a6d6cac87df8ff82e78f5b19ca
Timur <nftgameur989@gmail.com>
2026-03-10 09:02:07 +0100

    version1

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -7,8 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
 #[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
-class Utilisateur
+class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -105,4 +108,23 @@ class Utilisateur
 
         return $this;
     }
+
+    public function getUserIdentifier(): string
+{
+    return $this->email;
+}
+
+public function getRoles(): array
+{
+    return ['ROLE_USER'];
+}
+
+public function getPassword(): string
+{
+    return $this->mdp;
+}
+
+public function eraseCredentials(): void
+{
+}
 }
