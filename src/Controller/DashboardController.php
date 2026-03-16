f4fe47b423c04f20b065f47923236b13abec9143
Jenzoov <enzovct22@gmail.com>
2026-03-16 11:20:34 +0100

    page de gestion des étudiants V1

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,9 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Repository\EtudiantRepository;
 
 class DashboardController extends AbstractController
 {
+    //     #[Route('/page-de-test', name: 'app_page_de_test')]
+//     public function pageDeTest(): Response
+//     {
+//         return $this->render('pageDeTest.html.twig', [
+//     'etudiants' => [] // On envoie une liste vide, l'erreur disparaîtra
+// ]);
+//     }
+    #[Route('/etudiants', name: 'app_etudiant_index')]
+    public function index(EtudiantRepository $etudiantRepository): Response
+    {
+        // 1. On récupère tous les étudiants en base de données
+        $listeEtudiants = $etudiantRepository->findAll();
+
+        // 2. On "envoie" la variable au Twig
+        return $this->render('pageDeTest.html.twig', [
+            'etudiants' => $listeEtudiants, // C'est ici que la variable est créée !
+        ]);
+    }
+
+
     #[Route('/menu-test', name: 'app_menu_test')]
     public function menuTest(): Response
     {
